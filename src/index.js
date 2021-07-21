d2ae2a7805eb2cd9b1b1a1aa490d649600af1046
Gabriel Lucian Nestor <lucian.nestor@gmail.com>
2021-07-21 14:49:01 +0300

    Using 3 variables

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
 import "./styles.css";
+
+const input = document.querySelectorAll(".controls input");
+
+function handleUpdate() {
+  const suffix = this.dataset.sizing || "";
+  //data set puts all the elements that contain "data" in their name and put them in an object
+  document.documentElement.style.setProperty(
+    `--${this.name}`,
+    this.value + suffix
+  );
+}
+
+input.forEach((input) => input.addEventListener("change", handleUpdate));
+input.forEach((input) => input.addEventListener("mousemove", handleUpdate));
